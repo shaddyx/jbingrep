d054e28e094f7b7f4a515553c79dd6aab425d9d4
ayakushko <ayakushko@intropro.com>
2017-03-02 00:21:55 +0200

    [-] some fixes

--- src/main/java/ua/org/shaddy/bingrep/grammar/rules/RulesAnalyzer.java
+++ src/main/java/ua/org/shaddy/bingrep/grammar/rules/RulesAnalyzer.java
@@ -9,46 +9,54 @@ import ua.org.shaddy.bingrep.grammar.tokenizer.impl.OpenBraceToken;
 
 public class RulesAnalyzer {
 	private final TokenList tokens;
-	
-	public RulesAnalyzer(List<Token> tokens){
+
+	public RulesAnalyzer(List<Token> tokens) {
 		this.tokens = new TokenList(tokens);
 	}
-	
-	public RulesAnalyzer(TokenList tokens){
+
+	public RulesAnalyzer(TokenList tokens) {
 		this.tokens = tokens;
 	}
-	
+
 	public TokenList getTokens() {
 		return tokens;
 	}
-	
-	private TokenList getInnerTokens(Class<? extends Token> start, Class<? extends Token> end){
+
+	private TokenList getInnerTokens(Class<? extends Token> start, Class<? extends Token> end) {
 		List<Token> tokenList = new LinkedList<Token>();
-		if (!start.isInstance(tokens.getNext())){
+		if (!start.isInstance(tokens.getNext())) {
 			throw new RulesAnalyzerException("Error no start token found:" + start.getName());
 		}
 		tokens.poll();
 		int startTokens = 1;
-		while (!end.isInstance(tokens.getNext()) || startTokens != 1){
-			if (start.isInstance(tokens.getNext())){
-				startTokens ++;
-			} else if (end.isInstance(tokens.getNext())){
-				startTokens --;
-			} 
+		while (!end.isInstance(tokens.getNext()) || startTokens != 1) {
+			if (start.isInstance(tokens.getNext())) {
+				startTokens++;
+			} else if (end.isInstance(tokens.getNext())) {
+				startTokens--;
+			}
 			tokenList.add(tokens.poll());
 		}
 		return new TokenList(tokenList);
 	}
-	
+
 	private List<GrepRule> getInnerRules(Class<OpenBraceToken> class1, Class<CloseBraceToken> class2) {
 		TokenList innerTokens = getInnerTokens(class1, class2);
 		RulesAnalyzer rulesAnalyzer = new RulesAnalyzer(innerTokens);
+		LinkedList<GrepRule> rules = new LinkedList<GrepRule>();
+		GrepRule rule;
+		while ((rule = rulesAnalyzer.analyze())!= null){
+			rules.add(rule);
+		}
+		return rules;
 	}
-	
-	public GrepRule analyze(){
-		if (tokens.getNext() instanceof OpenBraceToken){
+
+	public GrepRule analyze() {
+		if (tokens.getNext() instanceof OpenBraceToken) {
 			List<GrepRule> rulesList = getInnerRules(OpenBraceToken.class, CloseBraceToken.class);
 		}
+		return null;
+
 	}
-	
+
 }
--- src/main/java/ua/org/shaddy/bingrep/grammar/rules/impl/CheckByteRule.java
+++ src/main/java/ua/org/shaddy/bingrep/grammar/rules/impl/CheckByteRule.java
@@ -1,5 +1,16 @@
 package ua.org.shaddy.bingrep.grammar.rules.impl;
 
-public class CheckByteRule {
+import ua.org.shaddy.bingrep.grammar.rules.GrepRule;
+
+public class CheckByteRule extends GrepRule {
+	private int value;
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
 	
 }
